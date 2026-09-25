2a2c926bf63fa258bc669832fa8d22ed4696fe99
charu-sopra <charu.pathak@soprasteria.com>
2026-09-25 11:57:20 +0530

    signup ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Signup } from './signup/signup';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
-  imports: [RouterOutlet, Signup],
+  imports: [RouterOutlet, Signup, Header, Footer ],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,44 +1,110 @@
-import { NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormControl,FormGroup,  ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl,FormGroup,  FormsModule,  ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../services/user-service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 
+
+interface Role {
+  value: string;
+  viewValue: string;
+} 
 @Component({
-  imports: [ReactiveFormsModule],
+  imports: [MatStepperModule, MatIconModule, MatFormFieldModule, MatSelectModule, FormsModule , ReactiveFormsModule, MatButtonModule,  MatInputModule ],
   selector: 'app-signup',
   styleUrl: './signup.scss',
   templateUrl: './signup.html',
 })
 export class Signup {
 
+  hidePassword = true; 
+  hideConfirmPassword = true;
+
+roles: Role[] = [
+  { value: 'SUPERUSER', viewValue: 'SUPER USER' },
+  { value: 'ANALYSER', viewValue: 'ANALYSER' },
+  { value: 'APPROVER', viewValue: 'APPROVER' },
+  { value: 'GUEST', viewValue: 'GUEST' }
+];
+
   private userService = inject(UserService);
 
   signUpForm = new FormGroup({
-    name : new FormControl('', [Validators.required]), //u can set default values by writing the name here itself and set validators
-    email : new FormControl('', [Validators.pattern("^[A-Za-z0-9._%+-]+@soprasteria\\.com$"), Validators.required]),
-    password : new FormControl('', [Validators.minLength(12), Validators.required])
+
+    employeeId: new FormControl('', [Validators.required]),
+
+    firstName: new FormControl('', [Validators.required]),
+
+    middleName: new FormControl(''), 
+    
+    lastName: new FormControl('', [Validators.required]),
+
+    phoneNo: new FormControl('', [Validators.required,
+                                  Validators.pattern('^[6-9][0-9]{9}$')]),
+
+    email: new FormControl('', [Validators.required,
+                                Validators.email,
+                                Validators.pattern('^[A-Za-z0-9._%+-]+@soprasteria\\.com$')]),
+
+    designation: new FormControl('', [Validators.required]),
+
+    userRole: new FormControl('', [Validators.required]),
+
+    password: new FormControl('', [Validators.required,
+                                  Validators.minLength(12)]),
+
+    confirmPassword: new FormControl('', [Validators.required])
   });
 
   createUser() {
-
-  const formData = this.signUpForm.value;
-  const request = this.userService.createUser(formData);
-
-  request.subscribe((response) => {
-    console.log('Signup successful:', response);
+    const formData = this.signUpForm.value;
+    const request = this.userService.createUser(formData);
+    request.subscribe((response) => {console.log('Signup successful:', response);
   });
 
   }
+get employeeId() {
+  return this.signUpForm.controls.employeeId;
+}
 
-  get name() {
-  return this.signUpForm.controls.name;
-  }
+get firstName() {
+  return this.signUpForm.controls.firstName;
+}
 
-  get email() {
+get middleName() {
+  return this.signUpForm.controls.middleName;
+}
+
+get lastName() {
+  return this.signUpForm.controls.lastName;
+}
+
+get phoneNo() {
+  return this.signUpForm.controls.phoneNo;
+}
+
+get email() {
   return this.signUpForm.controls.email;
-  }
-  get password() {
+}
+
+get designation() {
+  return this.signUpForm.controls.designation;
+}
+
+get userRole() {
+  return this.signUpForm.controls.userRole;
+}
+
+get password() {
   return this.signUpForm.controls.password;
-  }
+}
+
+get confirmPassword() {
+  return this.signUpForm.controls.confirmPassword;
+}
 
 }
